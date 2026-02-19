26fc001da90089d68e17582b4fecb6b15c278d20
Giacomo <playgiacomo@gmail.com>
2026-02-19 18:54:47 +0100

    Initial commit

---
+++ FinanceManager/FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by Giacomo Grieco on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
